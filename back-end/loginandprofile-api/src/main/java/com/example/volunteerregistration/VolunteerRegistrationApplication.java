0d02097bd78e8ff6d435e542355cb6b7bf9735a1
YuanG0319tt <149190045+YuanG0319tt@users.noreply.github.com>
2025-03-07 15:12:56 -0600

    Add files via upload

---
+++ back-end/loginandprofile-api/src/main/java/com/example/volunteerregistration/VolunteerRegistrationApplication.java
@@ -0,0 +1,11 @@
+package com.example.volunteerregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerRegistrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerRegistrationApplication.class, args);
+    }
+}
