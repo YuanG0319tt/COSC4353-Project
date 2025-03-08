37b6cffc21abd379e609b2e8e808b09d1a4e9fa5
zoeyeballard <zoeyeballard@gmail.com>
2025-03-07 23:48:31 -0600

    fixed volunteer history and notifications by adding dependencies

--- back-end/match-api/src/main/java/com/example/volunteerMatching/repositories/VolHistoryRepository.java
+++ back-end/match-api/src/main/java/com/example/volunteerMatching/repositories/VolHistoryRepository.java
@@ -1,8 +1,8 @@
 package com.example.volunteerMatching.repositories;
 
-import com.example.volunteerMatching.models.VolHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.volunteerMatching.models.VolHistory;
 
 @Repository
 public interface VolHistoryRepository extends JpaRepository<VolHistory, Long> {
