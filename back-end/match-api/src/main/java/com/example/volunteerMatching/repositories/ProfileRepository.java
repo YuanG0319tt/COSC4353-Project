e26b7d82f73a55c5e7d13ab16996542759fa0394
YuanG0319tt <149190045+YuanG0319tt@users.noreply.github.com>
2025-03-21 15:30:44 -0500

    Add files via upload

---
+++ back-end/match-api/src/main/java/com/example/volunteerMatching/repositories/ProfileRepository.java
@@ -0,0 +1,11 @@
+//package com.example.volunteerregistration.repository;
+//
+//import com.example.volunteerregistration.model.UserProfile;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ProfileRepository extends JpaRepository<UserProfile, Long> {
+//    // You can define custom query methods here if needed, e.g.:
+//    // Optional<UserProfile> findByFullName(String fullName);
+//}
