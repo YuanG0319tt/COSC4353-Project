d53a8979aa86cbc410ce24d56a9e4fbf4aa7079d
zoeyeballard <zoeyeballard@gmail.com>
2025-03-21 16:12:19 -0500

    updated dotenvconfig

--- back-end/match-api/src/main/java/com/example/volunteerMatching/config/DotenvConfig.java
+++ back-end/match-api/src/main/java/com/example/volunteerMatching/config/DotenvConfig.java
@@ -1,4 +1,5 @@
 import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Map;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -7,7 +8,7 @@ import javax.annotation.PostConstruct;
 public class DotenvConfig {
     @PostConstruct
     public void loadEnvVariables() {
-        Dotenv dotenv = Dotenv.load();
+        Map<String, String> dotenv = System.getenv();
         System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
         System.setProperty("DB_USER", dotenv.get("DB_USER"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
