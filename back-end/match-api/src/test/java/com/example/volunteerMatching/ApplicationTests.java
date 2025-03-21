0873928d2d6d597628b051c85029b2666848e5c2
Jasmine Garcia <garciajasmine537@gmail.com>
2025-03-21 17:52:44 -0500

    Update ApplicationTests.java

--- back-end/match-api/src/test/java/com/example/volunteerMatching/ApplicationTests.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/ApplicationTests.java
@@ -2,13 +2,11 @@ package com.example.volunteerMatching;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-// @SpringBootTest(properties = "spring.config.location=classpath:application-test.properties")
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
