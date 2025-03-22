9ff5e4ebbdda1228dfc49394f82258d9bc6bf7cb
Jasmine Garcia <garciajasmine537@gmail.com>
2025-03-21 20:08:44 -0500

    Update VolHistoryTest.java
    test volunteer setters

--- back-end/match-api/src/test/java/com/example/volunteerMatching/VolHistoryTest.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/VolHistoryTest.java
@@ -17,4 +17,29 @@ public class VolHistoryTest {
         assertEquals(12, volHistory.getHoursVolunteered());
         assertEquals("Completed", volHistory.getStatus());
     }
+
+    @Test
+    public void testVolunteerSetters() {
+        List<String> skillsResult = Arrays.asList("Lifting", "Coding");
+        List<String> preferencesResult = Arrays.asList("Cleaning", "Food Pantry");
+        Volunteer volunteer = new Volunteer("Houston", "Jasmine", "2025-03-27", skillsResult, preferencesResult);
+
+        String newLocation = "Dallas";
+        String newName = "Molly";
+        String newAvailability = "2025-05-25";
+        List<String> newSkills = Arrays.asList("Cooking", "Teaching");
+        List<String> newPreferences = Arrays.asList("Outdoor", "Weekend");
+
+        volunteer.setLocation(newLocation);
+        volunteer.setName(newName);
+        volunteer.setAvailability(newAvailability);
+        volunteer.setSkills(newSkills);
+        volunteer.setPreferences(newPreferences);
+
+        assertEquals(newLocation, volunteer.getLocation());
+        assertEquals(newName, volunteer.getName());
+        assertEquals(newAvailability, volunteer.getAvailability());
+        assertEquals(newSkills, volunteer.getSkills());
+        assertEquals(newPreferences, volunteer.getPreferences());
+    }
 }
