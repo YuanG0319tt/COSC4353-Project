6b4b09af1c23f015b4d953ec59c798d34e3fa72b
Jasmine Garcia <garciajasmine537@gmail.com>
2025-03-21 20:27:03 -0500

    Update UserProfileTest.java
    test setters for user profile

--- back-end/match-api/src/test/java/com/example/volunteerMatching/UserProfileTest.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/UserProfileTest.java
@@ -36,4 +36,49 @@ public class UserProfileTest {
         assertEquals("Cleaning", profile.getPreferences());
         assertIterableEquals(availabilityResult, profile.getAvailability());
     }
+
+    @Test
+    public void testUserProfileSetters() {
+        List<String> skillsResult = Arrays.asList("Lifting", "Organizing");
+        List<String> skillsSetter = Arrays.asList("Sweeping", "Coding");
+
+        LocalDate start = LocalDate.of(2025, 5, 19);
+        LocalDate end = LocalDate.of(2025, 5, 20);
+        List<LocalDate> availabilityResult = start.datesUntil(end.plusDays(1), Period.ofDays(1)).toList();
+        LocalDate startSetter = LocalDate.of(2025, 5, 10);
+        LocalDate endSetter = LocalDate.of(2025, 5, 19);
+        List<LocalDate> availabilitySetter = startSetter.datesUntil(endSetter.plusDays(1), Period.ofDays(1)).toList();
+
+        UserProfile profile = new UserProfile("John Smith", "123 Easy Street", "Apt. 123", "Houston", "Texas", "77029", skillsResult, "Cleaning", availabilityResult);
+
+        String newName = "Jane Smith";
+        String newAddress1 = "321 Hard Lane";
+        String newAddress2 = "Apt. 321";
+        String newCity = "New Orleans";
+        String newState = "Louisiana";
+        String newZip = "12378";
+        List<String> newSkills = skillsSetter;
+        String newPreferences = "Sorting";
+        List<LocalDate> newAvailability = availabilitySetter;
+
+        profile.setFullName(newName);
+        profile.setAddress1(newAddress1);
+        profile.setAddress2(newAddress2);
+        profile.setCity(newCity);
+        profile.setState(newState);
+        profile.setZip(newZip);
+        profile.setSkills(newSkills);
+        profile.setPreferences(newPreferences);
+        profile.setAvailability(newAvailability);
+
+        assertEquals(newName, profile.getFullName());
+        assertEquals(newAddress1, profile.getAddress1());
+        assertEquals(newAddress2, profile.getAddress2());
+        assertEquals(newCity, profile.getCity());
+        assertEquals(newState, profile.getState());
+        assertEquals(newZip, profile.getZip());
+        assertIterableEquals(newSkills, profile.getSkills());
+        assertEquals(newPreferences, profile.getPreferences());
+        assertIterableEquals(newAvailability, profile.getAvailability());
+    }
 }
