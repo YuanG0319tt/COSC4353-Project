5f1c50c78ec85b8718ed7202eb2818de11995144
Jasmine Garcia <garciajasmine537@gmail.com>
2025-03-21 19:40:05 -0500

    Update DatabaseConnection.java

--- back-end/match-api/src/main/java/com/example/volunteerMatching/config/DatabaseConnection.java
+++ back-end/match-api/src/main/java/com/example/volunteerMatching/config/DatabaseConnection.java
@@ -1,11 +1,13 @@
+package com.example.volunteerMatching.config;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://your-rds-endpoint:3306/yourDatabase";
-    private static final String USER = "yourUsername";
-    private static final String PASSWORD = "yourPassword";
+    private static final String URL = "jdbc:mysql://nabadatabase.cliacqmekc7s.us-east-2.rds.amazonaws.com:3306/nabaDatabase";
+    private static final String USER = "root";
+    private static final String PASSWORD = "nabaTest76!";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
