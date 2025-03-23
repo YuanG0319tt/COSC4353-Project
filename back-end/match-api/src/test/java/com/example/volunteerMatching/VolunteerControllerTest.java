f9a2f6aa2f2d71bf0abe39cd5adce9c8adbf5a66
Jasmine Garcia <garciajasmine537@gmail.com>
2025-03-22 23:14:51 -0500

    Update VolunteerControllerTest.java

--- back-end/match-api/src/test/java/com/example/volunteerMatching/VolunteerControllerTest.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/VolunteerControllerTest.java
@@ -1,5 +1,6 @@
-package com.example.volunteerMatching.controllers;
+package com.example.volunteerMatching;
 
+import com.example.volunteerMatching.controllers.VolunteerController;
 import com.example.volunteerMatching.models.Volunteer;
 import com.example.volunteerMatching.services.VolunteerService;
 import org.junit.jupiter.api.BeforeEach;
