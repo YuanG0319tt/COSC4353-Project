dbc7801078f12cd2167eced5bfc81c03979f1db1
Jasmine Garcia <garciajasmine537@gmail.com>
2025-03-22 23:15:35 -0500

    Update VolunteerTest.java

--- back-end/match-api/src/test/java/com/example/volunteerMatching/VolunteerTest.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/VolunteerTest.java
@@ -1,5 +1,6 @@
-package com.example.volunteerMatching.models;
+package com.example.volunteerMatching;
 
+import com.example.volunteerMatching.models.Volunteer;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
@@ -18,5 +19,30 @@ public class VolunteerTest {
         assertEquals("2025-03-27", volunteer.getAvailability());
         assertIterableEquals(skillsResult, volunteer.getSkills());
         assertIterableEquals(preferencesResult, volunteer.getPreferences());
+    }
+
+    @Test
+    public void testVolunteerSetters() {
+        List<String> skillsResult = Arrays.asList("Lifting", "Coding");
+        List<String> preferencesResult = Arrays.asList("Cleaning", "Food Pantry");
+        Volunteer volunteer = new Volunteer("Houston", "Jasmine", "2025-03-27", skillsResult, preferencesResult);
+
+        String newLocation = "Dallas";
+        String newName = "Molly";
+        String newAvailability = "2025-05-25";
+        List<String> newSkills = Arrays.asList("Cooking", "Teaching");
+        List<String> newPreferences = Arrays.asList("Outdoor", "Weekend");
+
+        volunteer.setLocation(newLocation);
+        volunteer.setName(newName);
+        volunteer.setAvailability(newAvailability);
+        volunteer.setSkills(newSkills);
+        volunteer.setPreferences(newPreferences);
+
+        assertEquals(newLocation, volunteer.getLocation());
+        assertEquals(newName, volunteer.getName());
+        assertEquals(newAvailability, volunteer.getAvailability());
+        assertEquals(newSkills, volunteer.getSkills());
+        assertEquals(newPreferences, volunteer.getPreferences());
     }
 }
