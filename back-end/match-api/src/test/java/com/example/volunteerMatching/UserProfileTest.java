9163e4d56a100c1f6e44569b617ce32bc53582b0
Jasmine Garcia <garciajasmine537@gmail.com>
2025-03-22 23:13:36 -0500

    Update UserProfileTest.java

--- back-end/match-api/src/test/java/com/example/volunteerMatching/UserProfileTest.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/UserProfileTest.java
@@ -1,9 +1,10 @@
-package com.example.volunteerMatching.models;
+package com.example.volunteerMatching;
 
+import com.example.volunteerMatching.models.UserProfile;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
+// import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
@@ -15,17 +16,12 @@ public class UserProfileTest {
     void testUserProfileConstructor() {
         List<String> skillsResult = Arrays.asList("Lifting", "Organizing");
 
-        // Define start and end dates for availability
         LocalDate start = LocalDate.of(2025, 5, 19);
         LocalDate end = LocalDate.of(2025, 5, 20);
-
-        // Generate availability list
         List<LocalDate> availabilityResult = start.datesUntil(end.plusDays(1), Period.ofDays(1)).toList();
 
-        // Create the profile
         UserProfile profile = new UserProfile("John Smith", "123 Easy Street", "Apt. 123", "Houston", "Texas", "77029", skillsResult, "Cleaning", availabilityResult);
 
-        // Assertions
         assertEquals("John Smith", profile.getFullName());
         assertEquals("123 Easy Street", profile.getAddress1());
         assertEquals("Apt. 123", profile.getAddress2());
