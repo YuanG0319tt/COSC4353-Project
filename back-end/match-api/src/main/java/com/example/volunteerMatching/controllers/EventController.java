5f37665c9fa849eab5e861bc99f3507ae5ccab62
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-16 17:26:31 -0500

    Update EventController.java

--- back-end/match-api/src/main/java/com/example/volunteerMatching/controllers/EventController.java
+++ back-end/match-api/src/main/java/com/example/volunteerMatching/controllers/EventController.java
@@ -3,8 +3,6 @@ package com.example.volunteerMatching.controllers;
 import com.example.volunteerMatching.models.EventDetails;
 import com.example.volunteerMatching.services.EventService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
