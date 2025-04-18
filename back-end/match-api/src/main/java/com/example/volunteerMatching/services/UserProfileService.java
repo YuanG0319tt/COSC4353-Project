05ddb37faa8d5d52f7ed8fd6e04804b8b6c47c52
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-18 09:10:38 -0500

    Add files via upload

---
+++ back-end/match-api/src/main/java/com/example/volunteerMatching/services/UserProfileService.java
@@ -0,0 +1,8 @@
+package com.example.volunteerMatching.services;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.volunteerMatching.models.UserProfile;
+
+public interface UserProfileService extends IService<UserProfile> {
+
+}
