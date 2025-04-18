5dffa898c957a6a87c99a3a2f01a7a54c26cc126
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-18 09:08:21 -0500

    Update VolunteerHistoryRepository.java

--- back-end/match-api/src/main/java/com/example/volunteerMatching/repositories/VolunteerHistoryRepository.java
+++ back-end/match-api/src/main/java/com/example/volunteerMatching/repositories/VolunteerHistoryRepository.java
@@ -2,20 +2,9 @@ package com.example.volunteerMatching.repositories;
 
 import com.example.volunteerMatching.models.VolunteerHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface VolunteerHistoryRepository extends JpaRepository<VolunteerHistory, Long> {
-    
-    // This query joins the tables to get all the data you need
-    @Query("SELECT vh FROM VolunteerHistory vh " +
-           "JOIN FETCH vh.userCredentials uc " +
-           "JOIN FETCH uc.userInfo ui " +
-           "JOIN FETCH vh.eventDetails ed")
-    List<VolunteerHistory> findAllWithUserAndEventDetails();
-    
-    // Or find by user ID
-    List<VolunteerHistory> findByUserCredentials_UserId(Long userId);
 }
