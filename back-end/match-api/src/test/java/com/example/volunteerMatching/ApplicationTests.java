d69ca535a425f7357f2be1dc0e1b59e52765004c
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-18 09:11:32 -0500

    Update ApplicationTests.java

--- back-end/match-api/src/test/java/com/example/volunteerMatching/ApplicationTests.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/ApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.volunteerMatching;
 
+// import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class ApplicationTests {
+
     @Test
     void contextLoads() {
+        System.out.println("Spring context loaded ✅");
     }
 }
