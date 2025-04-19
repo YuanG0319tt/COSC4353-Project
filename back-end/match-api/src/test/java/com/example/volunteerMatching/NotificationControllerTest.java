21a47587c929b07c59229dc0967e4720a44561c6
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-19 16:46:02 -0500

    Update NotificationControllerTest.java

--- back-end/match-api/src/test/java/com/example/volunteerMatching/NotificationControllerTest.java
+++ back-end/match-api/src/test/java/com/example/volunteerMatching/NotificationControllerTest.java
@@ -8,17 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //import com.fasterxml.jackson.databind.ObjectMapper;
