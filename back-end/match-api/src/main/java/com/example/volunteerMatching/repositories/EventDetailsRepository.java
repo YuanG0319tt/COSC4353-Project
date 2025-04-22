98443dfd7738e5db66dfd345547ad9a59e881d3e
YuanG0319tt <149190045+YuanG0319tt@users.noreply.github.com>
2025-04-22 13:47:53 -0500

    Delete back-end/match-api directory

--- back-end/match-api/src/main/java/com/example/volunteerMatching/repositories/EventDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.volunteerMatching.repositories;
-
-import com.example.volunteerMatching.models.EventDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventDetailsRepository extends JpaRepository<EventDetails, Integer> {
-}
