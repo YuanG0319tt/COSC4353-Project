b42c457df93ddf5b02d639078c3ea2480788e001
YuanG0319tt <149190045+YuanG0319tt@users.noreply.github.com>
2025-04-23 02:30:36 -0500

    Delete src/main directory

--- src/main/java/com/example/demo/repositories/EventDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entity.EventDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventDetailsRepository extends JpaRepository<EventDetails, Integer> {
-}
--- src/main/java/com/example/demo/utils/StringUtil.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.utils;
-
-public class StringUtil {
-
-    public static boolean isNotNullOrEmpty(String str) {
-        return str != null && !str.equals("");
-    }
-}
