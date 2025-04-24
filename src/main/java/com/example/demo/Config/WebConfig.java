75d66350ec75314eecddc08aff24d7bbe2355c07
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-23 21:34:26 -0500

    Update WebConfig.java

--- src/main/java/com/example/demo/Config/WebConfig.java
+++ src/main/java/com/example/demo/Config/WebConfig.java
@@ -2,7 +2,9 @@ package com.example.demo.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Path to your front-end directory (adjust if needed)
         String externalPath = Paths.get("front-end").toAbsolutePath().toUri().toString();
 
         registry.addResourceHandler("/**")
