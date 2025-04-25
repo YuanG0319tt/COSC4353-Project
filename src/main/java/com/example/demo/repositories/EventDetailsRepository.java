39f8e533064ce1ee324bb97360d0b26011eedcfd
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-25 02:49:25 -0500

    Update EventDetailsRepository.java

--- src/main/java/com/example/demo/repositories/EventDetailsRepository.java
+++ src/main/java/com/example/demo/repositories/EventDetailsRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.repositories;
 import com.example.demo.entity.EventDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
 public interface EventDetailsRepository extends JpaRepository<EventDetails, Integer> {
+    Optional<EventDetails> findByEventNameIgnoreCase(String eventName);
 }
