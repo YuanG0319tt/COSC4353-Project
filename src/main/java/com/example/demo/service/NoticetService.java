e40e66451c95d4f45444ebfc3aae6f8124051a8e
Jasmine Garcia <garciajasmine537@gmail.com>
2025-04-24 22:57:43 -0500

    Update NoticetService.java

--- src/main/java/com/example/demo/service/NoticetService.java
+++ src/main/java/com/example/demo/service/NoticetService.java
@@ -1,9 +1,8 @@
 package com.example.demo.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.demo.entity.Event;
 import com.example.demo.entity.Notice;
 
 public interface NoticetService extends IService<Notice> {
-
+    boolean removeById(Long id);
 }
